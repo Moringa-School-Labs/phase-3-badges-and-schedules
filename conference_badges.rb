a680dbb1d0081bc191b717ebb678e830551b1e8e
catherine <vuthi.esther@gmail.com>
2022-06-08 19:47:14 +0300

    completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator speakers
+    speakers.map do |speaker|
+     "Hello, my name is #{speaker}."
+    end
+end
+
+def assign_rooms speakers
+    speakers.map.with_index(1) do | speaker, room_no|
+        "Hello, #{speaker}! You'll be assigned to room #{room_no}!"
+    end
+end
+
+def printer speakers
+    batch_badge_creator(speakers).each do | badge |
+        puts badge
+    end
+
+    assign_rooms(speakers).each do |assigned_room|
+        puts assigned_room
+    end
+    
+end
